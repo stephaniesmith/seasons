d6700789947dea573db32c88b0a95f34fabfe389
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 11:52:49 -0800

    components

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SeasonDisplay = props => {
+  return (
+    <div>
+      Season display
+    </div>
+  );
+};
+
+SeasonDisplay.propTypes = {
+
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello There!</div>
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.error(err)
+    );
+    return <div>Latitude: </div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));

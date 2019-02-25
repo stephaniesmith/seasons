f6aa10a120a1129ca288f6fa7e580479baf8aa7d
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 18:12:33 -0800

    getting the linter working

--- src/index.js
+++ src/index.js
@@ -3,30 +3,30 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends Component {
-	state = {
-		lat: null,
-		errorMessage: ''
-	}
-	
-	componentDidMount() {
-		window.navigator.geolocation.getCurrentPosition(
-			({ coords }) => this.setState({ lat: coords.latitude }),
-			({ message }) => this.setState({ errorMessage: message})
-		);
+state = {
+  lat: null,
+  errorMessage: ''
+};
 
-	}
+componentDidMount() {
+  window.navigator.geolocation.getCurrentPosition(
+    ({ coords }) => this.setState({ lat: coords.latitude }),
+    ({ message }) => this.setState({ errorMessage: message })
+  );
 
-	render() {
-		const { lat, errorMessage } = this.state;
-		
-		return (
-			<div>
-				{lat && <SeasonDisplay lat={lat}/>}
-				{errorMessage && <p>Error: {errorMessage}</p>}
-				{!lat && !errorMessage && <p>Loading...</p>}
-			</div>
-		);
-	}
+}
+
+render() {
+  const { lat, errorMessage } = this.state;
+
+  return (
+    <div>
+      {lat && <SeasonDisplay lat={lat}/>}
+      {errorMessage && <p>Error: {errorMessage}</p>}
+      {!lat && !errorMessage && <p>Loading...</p>}
+    </div>
+  );
+}
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));

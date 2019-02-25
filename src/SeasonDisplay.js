dcfc34ac26919d32bc5f9d3a9517fbd7aa8d1eb0
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 18:07:31 -0800

    css and eslint

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -10,7 +11,7 @@ const seasonConfig = {
     text: 'Burr, it is chilly',
     iconName: 'snowflake'
   }
-}
+};
 
 const getSeason = (lat, month) => {
   if(month > 2 && month < 9) {
@@ -27,9 +28,9 @@ const SeasonDisplay = ({ lat }) => {
 
   return (
     <div>
-      <i className={`massive ${iconName} icon`}></i>
+      <i className={`icon-left massive ${iconName} icon`}></i>
       <h1>{text}</h1>
-      <i className={`massive ${iconName} icon`}></i>
+      <i className={`icon-right massive ${iconName} icon`}></i>
     </div>
   );
 };

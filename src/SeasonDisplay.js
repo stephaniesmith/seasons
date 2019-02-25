6fbfa6694d9bb9d1a7d992c11f7c70a7faa6fad9
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 17:51:37 -0800

    refactor

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if(month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -12,20 +23,19 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = ({ lat }) => {
   const month = new Date().getMonth();
   const season = getSeason(lat, month);
-
-  console.log(season);
+  const { text, iconName } = seasonConfig[season];
 
   return (
     <div>
-      {season === 'winter'
-        ? 'Burr, it is chilly'
-        : 'Lets hit the beach'}
+      <i className={`massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`massive ${iconName} icon`}></i>
     </div>
   );
 };
 
 SeasonDisplay.propTypes = {
-
+  lat: PropTypes.number
 };
 
 export default SeasonDisplay;
